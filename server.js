9de04f15a540a425ebb277b655a768662909e389
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-03-19 10:41:06 +0100

    lagt till undersidorna och länkat dessa

--- server.js
+++ server.js
@@ -4,14 +4,23 @@ const express = require("express"); //inkludera express
 const app = express();//denna variabeln startar upp applikationen
 const port = 3000;//tilldela en port
 
-app.set("view engine", "ejs"); //view engine = ejs
+app.set("view engine", "ejs"); //view engine satt till ejs
 
-app.use(express.static("public")); //kunna använda statiska filer
+app.use(express.static("public")); //för att kunna använda statiska filer. Läggs i mappen public
 
-//Routing -  här startar applikationen upp
+//Routing 
 app.get("/", (req, res) => {
 res.render("index");
 });
+
+app.get("/addcourse", (req, res) => {
+res.render("addcourse");
+});
+    
+app.get("/about", (req, res) => {
+res.render("about");
+});
+
 
 //kör igång applikationen
 app.listen(port, () => {
