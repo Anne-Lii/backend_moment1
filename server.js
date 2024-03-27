c6b1d2ce81470f4348cf7e8c63cb77a7af3b9aaf
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-03-27 15:03:10 +0100

    lagt till nodeignore fil, fin justerat och städat koden bara medan jag följt instruktioner

--- server.js
+++ server.js
@@ -2,23 +2,54 @@
 
 const express = require("express"); //inkludera express
 const app = express();//denna variabeln startar upp applikationen
-const port = 3000;//tilldela en port
+const port = process.env.port | 3000;//tilldela en port
+const bodyParser = require("body-parser");//kunna ta emot data från formulär
 
 app.set("view engine", "ejs"); //view engine satt till ejs
-
 app.use(express.static("public")); //för att kunna använda statiska filer. Läggs i mappen public
+app.use(bodyParser.urlencoded({ extended: true}));//
 
-//Routing 
+//Routing - skriver ut sökvägarna, returnerar vy
 app.get("/", (req, res) => {
-res.render("index");
+    const myCoursesArr = [
+        { 
+            id: "1",
+            code: "DT057G",
+            name: "Webbutveckling 1", 
+            syllabus: "https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT057G/",
+            progression: "A"
+        },
+        { 
+            id: "2",
+            code: "DT084G",
+            name: "Introduktion till programmering i JavaScript", 
+            syllabus: "https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT084G/",
+            progression: "A"
+        },
+        { 
+            id: "3",
+            code: "DT200G",
+            name: "Grafisk teknik för webb", 
+            syllabus: "https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT200G/",
+            progression: "A"
+        }
+    ];
+
+    res.render("index", {
+        myCoursesArr
+    });//här visas startsidan och array med objekt skickas med
 });
 
 app.get("/addcourse", (req, res) => {
+res.render("addcourse");//här visas addcourse sidan
+});
+
+app.post("/addcourse", (req, res) => {
 res.render("addcourse");
 });
     
 app.get("/about", (req, res) => {
-res.render("about");
+res.render("about");//här visas about sidan
 });
 
 
